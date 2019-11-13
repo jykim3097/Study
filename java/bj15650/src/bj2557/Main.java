3ce9286dee3a8a0be13a9830a1dd6b3b8e84ff18
jykim3097 <jykim3097@gmail.com>
2019-11-13 15:26:41 +0900

    [Add] bj15650

---
+++ java/bj15650/src/bj2557/Main.java
@@ -0,0 +1,21 @@
+package bj2557;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//bj2557
+		//System.out.println("Hello World!");
+		
+		//bj10718
+//		System.out.println("강한친구 대한육군");
+//		System.out.println("강한친구 대한육군");
+		
+		//bj10171
+		System.out.println("\\    /\\\r\n" + 
+		" )  ( ')\r\n" + 
+				"(  /  )\r\n" + 
+				" \\(__)|");
+	}
+
+}
