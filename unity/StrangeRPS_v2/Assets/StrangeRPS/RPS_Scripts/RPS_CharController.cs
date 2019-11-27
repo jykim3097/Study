acf1258dee7207d4975efeb6d16009a3f5782d7e
jykim3097 <jykim3097@gmail.com>
2019-11-27 18:14:00 +0900

    [ADD] Start StrangeRPS_v2

---
+++ unity/StrangeRPS_v2/Assets/StrangeRPS/RPS_Scripts/RPS_CharController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RPS_CharController : MonoBehaviour
+{
+    public GameObject Char;
+
+    void Start()
+    {
+        Char.transform.position = new Vector2(-1000f, 400f);
+    }
+
+    public void SetActiveChar()
+    {
+        Char.transform.position = new Vector2(0f, -2f);
+    }
+}
