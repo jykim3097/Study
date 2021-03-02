f3d3c22cf5ee498ccc5e4fba87c543e838f395de
jykim3097 <jykim3097@gmail.com>
2021-03-03 07:20:58 +0900

    [추가] 210226 이전 pass
    1. 11164 아스키코드 구하기
    2. 11720 숫자의 합 구하기
    3. 10809 알파벳 찾기
    4. 2675 문자열 반복

---
+++ Academy_210215/Baekjoon/src/_7_String/_1_11654.java
@@ -0,0 +1,18 @@
+package _7_String;
+import java.util.Scanner;
+
+public class _1_11654 {
+	public static void main(String[] args) {
+		/*
+		 * https://www.acmicpc.net/problem/11654
+		 * 아스키코드 구하기
+		 */
+		
+		Scanner scan = new Scanner(System.in);
+
+		String m = scan.next();
+		char c = m.charAt(0);
+		
+		System.out.println((int)c);
+	}
+}
