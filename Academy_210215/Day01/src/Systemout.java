9304560f122c146a7637526ebf805d03bbd7e6c7
jykim3097 <jykim3097@gmail.com>
2021-03-03 07:14:57 +0900

    [삭제]

--- Academy_210215/Day01/src/Systemout.java
+++
@@ -1,9 +0,0 @@
-
-public class Systemout {
-
-	public static void main(String[] args) {
-		System.out.printf("오늘은 %d년 %d월 %d일 입니다.", 2021, 2, 15);
-		System.out.printf ("\n파이 값은 %.2f입니다.", 3.14);
-		System.out.printf ("\n파이 값은 %10.2f입니다.", 3.14);
-	}
-}
