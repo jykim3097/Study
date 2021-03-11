55ac78d327e6b622c27d564229a11686a1b8030a
jykim3097 <jykim3097@gmail.com>
2021-03-12 08:34:01 +0900

    [수정] 메모리 초과 에러 ...ing
    1. 어디서 메모리를 잡아먹는 걸까,,,

--- Academy_210215/Baekjoon/src/_7_String/_6_1152.java
+++ Academy_210215/Baekjoon/src/_7_String/_6_1152.java
@@ -1,60 +1,54 @@
 package _7_String;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class _6_1152 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		/*
 		 * https://www.acmicpc.net/problem/1152
 		 * 단어의 개수
 		 * 문자와 띄어쓰기로 구성되어있는 문자열에서 단어의 개수 구하기
+		 * 
+		 * 개행은 길이가 0이지만, 띄어쓰기는 길이가 1이다.
 		 */
-		
-		Scanner scan = new Scanner(System.in);
-		String str = scan.nextLine();
 
-//		int cnt = 0;
-//		if(str.length() == 1 && str.charAt(0) == ' ') { // 띄어쓰기만 입력될 때
-//			System.out.println(0);
-//		} else if ( str.length() == 0) { // 엔터만 입력될 때
-//			System.out.println(0);
-//		}
-//		else{
-//			if(str.charAt(0) == ' ' || str.charAt(str.length()-1) == ' ') cnt--;
-//			
-//			for(int i=0; i<str.length(); i++) {
-//				//System.out.println(str.charAt(i));
-//				
-//				
-//				if(str.charAt(i) == ' ') {
-//					cnt++;
-//				}
-//			}
-//			System.out.println(cnt+1);
-//		}
-		
-		String[] sArr = new String[str.length()];
-		String s = "";
-		
-		int j=0;
-		for(int i=0; i<str.length(); i++) {
-			if(str.charAt(i) != ' ' || i == str.length()-1) { // 띄어쓰기가 아니라면
-				s += str.charAt(i);
-			} else {
-				sArr[j] = s;
-				s= "";
-				j++;
-			}
-		}
-		sArr[j] = s;
-		System.out.println(Arrays.toString(sArr));
-		
+		BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+		String str = rd.readLine();
+
+		int len = str.length();
 		int cnt = 0;
-		for(int i=0; i<sArr.length; i++) {
-			//여기만 잘 설정하면 된다...
-		}
-		
+		String s = "";
+
+		if(len == 0) { //개행만 한 경우
+			System.out.println(0);
+		} else if (len == 1) { 
+			if(str.charAt(0) == ' ') { // 띄어쓰기만 입력된 경우
+				System.out.println(0);
+			} else { //한 글자인 경우
+				System.out.println(1);
+			}
+		} else { // 그 외 모든 경우
+			es: for(int i=0; i<len; i++) {
+				if(str.charAt(i) != ' ') { // 띄어쓰기가 아니라면
+
+					if(i == str.length()-1) {
+						cnt++;
+						break es;
+					}
+					s += str.charAt(i);
+					
+				} else { //띄어쓰기를 만나면
+					if( i == 0) { //첫글자가 띄어쓰기인 경우
+						continue;
+					}
+					cnt++;
+					s = "";
+				}
+			}
 		System.out.println(cnt);
+		}
+
 	}
 }
