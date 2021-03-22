e50bbfd3fe7559c5b23edf3918e8acbb150c7176
jykim3097 <jykim3097@gmail.com>
2021-03-22 23:58:35 +0900

    [추가] API IO
    1. InputStream과 OutputStream
    2. Reader와 Writer
    3. Buffered~

---
+++ Academy_210215/_210322_h/src/_2_Quiz21/_2_FileQuiz02.java
@@ -0,0 +1,13 @@
+package _2_Quiz21;
+
+public class _2_FileQuiz02 {
+
+	public static void main(String[] args) {
+		/*
+		 * 1. BufferedWriter, Date클래스 이용해서 file 폴더 경로에 오늘 날짜 ex)20210322.txt 이름으로 파일을 쓴다 - 내일이면 날짜가 바뀌겠죠?
+		 * 2. 내용은 아무거나 작성하세요
+		 * 3. 파일을 정상적으로 썼다면 BufferedReader를 통해서 읽어들이세요
+		 * 
+		 */
+	}
+}
